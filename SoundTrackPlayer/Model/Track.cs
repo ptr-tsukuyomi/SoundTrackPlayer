11c6b50b6ceba10fdfdbf1adc24b6b7b1dd2a62a
ptr tsukuyomi <tsukuyomi@outlook.jp>
2026-02-24 22:36:10 +0900

    メタデータから楽曲タイトルを取得する機能を追加

--- SoundTrackPlayer/Model/Track.cs
+++ SoundTrackPlayer/Model/Track.cs
@@ -1,10 +1,5 @@
 using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SoundTrackPlayer.Model
 {
--- SoundTrackPlayer/Model/TrackFactory.cs
+++ SoundTrackPlayer/Model/TrackFactory.cs
@@ -1,14 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SoundTrackPlayer.Model
 {
     internal class TrackFactory
     {
         private static Dictionary<string, Track> FileOriginTracks = new();
+
+
+        static private TrackInfo LoadTrackInfo(string file_path)
+        {
+            var result = TagLibSharp2.Core.MediaFile.Read(file_path);
+            var info = new TrackInfo();
+            info.Title = Path.GetFileName(file_path);
+
+            if (result.IsSuccess && result.Tag is not null)
+            {
+                if (result.Tag.Title is string s)
+                {
+                    info.Title = s;
+                }
+            }
+
+            return info;
+        }
 
         static public Track LoadFromFile(string file_path, bool ignore_broken_config = false)
         {
@@ -37,7 +50,9 @@ namespace SoundTrackPlayer.Model
                 Source = source,
                 Config = config ?? new TrackConfig()
             };
-            track.Info.Title = Path.GetFileName(file_path);
+
+            //
+            track.Info = LoadTrackInfo(file_path);
 
             FileOriginTracks.Add(file_path, track);
             return track;
